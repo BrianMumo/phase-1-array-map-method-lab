64cb3617e989b170e27d0d85a01290e8385f23b0
Brian <mumobrian2746@gmail.com>
2024-10-03 07:05:37 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +12,19 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCase(str) {
+  const result = [];
+  const wordsArray = str.split(' ');
+  for (let i = 0; i < wordsArray.length; i++) {
+    if (typeof wordsArray[i][0] === 'string') {
+      const firstLetter = wordsArray[i][0].toUpperCase();
+      result.push(firstLetter + wordsArray[i].slice(1));
+    }
+  }
+  return result.join(' ');
 }
+
+const titleCased = () => {
+  return tutorials.map(titleCase);
+}
+titleCased;
